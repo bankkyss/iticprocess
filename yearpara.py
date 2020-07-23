ffa2c174cf639139b1ae532c1e1e701ce4707415
bankkyss <42342455+bankkyss@users.noreply.github.com>
2020-07-23 19:32:40 +0700

    master

--- yearpara.py
+++ yearpara.py
@@ -3,7 +3,7 @@ import multiprocessing
 import time
 import pandas as pd
 import os 
-from raw_funtion import funtion
+#from raw_funtion import funtion
 from os import listdir
 from os.path import isfile, join 
 from geopy.distance import great_circle
